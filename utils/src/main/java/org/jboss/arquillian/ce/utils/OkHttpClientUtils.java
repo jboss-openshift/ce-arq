63970ed5844d574aaf0f27797bb742911421f780
Ales Justin <ales.justin@gmail.com>
2015-11-10 22:12:16 +0100

    Fix cookie handling.

--- utils/src/main/java/org/jboss/arquillian/ce/utils/OkHttpClientUtils.java
+++ utils/src/main/java/org/jboss/arquillian/ce/utils/OkHttpClientUtils.java
@@ -23,8 +23,15 @@
 
 package org.jboss.arquillian.ce.utils;
 
-import java.net.CookieManager;
-import java.net.CookiePolicy;
+import java.io.IOException;
+import java.net.CookieHandler;
+import java.net.HttpCookie;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.squareup.okhttp.OkHttpClient;
 
@@ -34,11 +41,59 @@ import com.squareup.okhttp.OkHttpClient;
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public class OkHttpClientUtils {
+    private static final SimpleCookieHandler COOKIE_HANDLER = new SimpleCookieHandler();
 
     public static void applyCookieHandler(OkHttpClient httpClient) {
-        CookieManager cookieManager = new CookieManager();
-        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-        httpClient.setCookieHandler(cookieManager);
+        COOKIE_HANDLER.clear(); // reset
+        httpClient.setCookieHandler(COOKIE_HANDLER);
     }
 
+    /**
+     * Just copy cookies based on proxy path.
+     */
+    private static class SimpleCookieHandler extends CookieHandler {
+        private static final String _PROXY = "/proxy";
+        private Map<String, List<HttpCookie>> cookiesMap = new HashMap<>();
+
+        private void clear() {
+            cookiesMap.clear();
+        }
+
+        private static String path(URI uri) {
+            String path = uri.getPath();
+            int p = path.indexOf(_PROXY);
+            return path.substring(p + _PROXY.length());
+        }
+
+        public synchronized Map<String, List<String>> get(URI uri, Map<String, List<String>> requestHeaders) throws IOException {
+            String path = path(uri);
+            List<String> list = new ArrayList<>();
+            for (Map.Entry<String, List<HttpCookie>> entry : cookiesMap.entrySet()) {
+                if (path.startsWith(entry.getKey())) {
+                    for (HttpCookie cookie : entry.getValue()) {
+                        list.add(cookie.toString());
+                    }
+                }
+            }
+            return list.isEmpty() ? Collections.<String, List<String>>emptyMap() : Collections.singletonMap("Cookie", list);
+        }
+
+        public synchronized void put(URI uri, Map<String, List<String>> responseHeaders) throws IOException {
+            for (Map.Entry<String, List<String>> entry : responseHeaders.entrySet()) {
+                String headerKey = entry.getKey();
+                if (headerKey.equalsIgnoreCase("Set-Cookie") || headerKey.equalsIgnoreCase("Set-Cookie2")) {
+                    for (String headerValue : entry.getValue()) {
+                        for (HttpCookie cookie : HttpCookie.parse(headerValue)) {
+                            List<HttpCookie> cookies = cookiesMap.get(cookie.getPath());
+                            if (cookies == null) {
+                                cookies = new ArrayList<>();
+                                cookiesMap.put(cookie.getPath(), cookies);
+                            }
+                            cookies.add(cookie);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
